c36b460cd8e6e18c69793272b2e102cbf3903bae
SindhujaManickam <47665343+SindhujaManickam@users.noreply.github.com>
2020-02-12 18:45:57 +0530

    Delete opentoken.bat

--- opentoken.bat
+++
@@ -1,4 +0,0 @@
-set projectLocation=C:\Users\Administrator\eclipse-workspace1\LikemindsDemo\opentokentestng.xml
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\opentokentestng.xml
